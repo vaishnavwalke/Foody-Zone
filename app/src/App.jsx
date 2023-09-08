94514b90c88af9ec8dd4a75f5a0b2bf0cc093aa8
vaishnavwalke <84281792+vaishnavwalke@users.noreply.github.com>
2023-09-08 15:21:35 +0530

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,152 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import SearchResult from "./components/SearchResult/SearchResult";
+
+export const BASE_URL = "http://localhost:9000";
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [filteredData, setFilteredData] = useState(null);
+  const [seletecBtn, setSeletedBtn] = useState("all");
+  useEffect(() => {
+    const fetchFoodData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(BASE_URL);
+
+        const json = await response.json();
+
+        setData(json);
+        setFilteredData(json);
+        setLoading(false);
+      } catch (error) {
+        setError("unable to fetch data");
+      }
+    };
+
+    fetchFoodData();
+  }, []);
+
+  //console.log(data);
+  const searchFood = (e) => {
+    const searchValue = e.target.value;
+    console.log(searchValue);
+
+    if (searchValue === "") {
+      setFilteredData(null);
+    }
+    const filter = data?.filter((food) =>
+      food.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredData(filter);
+  };
+
+  const filteredFood = (type) => {
+    if (type === "all") {
+      setFilteredData(data);
+
+      setSeletedBtn("all");
+      return;
+    }
+    const filter = data?.filter((food) =>
+      food.type.toLowerCase().includes(type.toLowerCase())
+    );
+    setFilteredData(filter);
+    setSeletedBtn(type);
+  };
+  const filterBtns = [
+    {
+      name: "All",
+      type: "all",
+    },
+    { name: "Breakfast", type: "breakfast" },
+    { name: "Lunch", type: "lunch" },
+    { name: "Dinner", type: "dinner" },
+  ];
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+  if (loading) return <div>loading...</div>;
+
+  return (
+    <>
+      <Container>
+        <TopContainer>
+          <div className="logo">
+            <img src="/logo.svg" alt="logo" />
+          </div>
+          <div className="search">
+            <input onChange={searchFood} placeholder="Search Food...." />
+          </div>
+        </TopContainer>
+        <FilterContainer>
+          {filterBtns.map((value) => (
+            <Button
+              isSelected={seletecBtn === value.type}
+              key={value.name}
+              onClick={() => filteredFood(value.type)}
+            >
+              {value.name}
+            </Button>
+          ))}
+        </FilterContainer>
+      </Container>
+      <SearchResult data={filteredData} />
+    </>
+  );
+};
+
+export default App;
+
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+const TopContainer = styled.section`
+  height: 140px;
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+  align-items: center;
+
+  .search {
+    input {
+      background-color: transparent;
+      border: 1px solid red;
+      color: white;
+      border-radius: 5px;
+      height: 40px;
+      font-size: 16px;
+      padding: 0 10px;
+      &::placeholder {
+        color: white;
+      }
+    }
+  }
+
+  @media (0<width <600px) {
+    flex-direction: column;
+    height: 120px;
+  }
+`;
+const FilterContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+  padding: 40px;
+`;
+export const Button = styled.button`
+  border-radius: 5px;
+  background: ${({ isSelected }) => (isSelected ? "#ff4393" : "#ff4343")};
+  outline: 1px solid ${({ isSelected }) => (isSelected ? "white " : "#ff4343")};
+  padding: 6px 12px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #ff4393;
+  }
+`;
---
+++ app/src/components/SearchResult/SearchResult.jsx
@@ -0,0 +1,96 @@
+import { styled } from "styled-components";
+import { BASE_URL, Button, Container } from "../../App";
+
+const SearchResult = ({ data }) => {
+  return (
+    <FoodCardContainer>
+      <Container>
+        <FoodCards>
+          {data?.map(({ name, image, text, price }) => (
+            <FoodCard key={name}>
+              <div className="food_image">
+                <img src={BASE_URL + image} />
+              </div>
+              <div className="food_info">
+                <div className="info">
+                  <h3>{name}</h3>
+                  <p>{text}</p>
+                </div>
+                <Button>${price.toFixed(2)}</Button>
+              </div>
+            </FoodCard>
+          ))}
+        </FoodCards>
+      </Container>
+    </FoodCardContainer>
+  );
+};
+
+export default SearchResult;
+const FoodCardContainer = styled.section`
+  min-height: calc(100vh - 210px);
+  background-image: url("/bg.png");
+  background-size: cover;
+`;
+
+const FoodCards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  row-gap: 32px;
+  column-gap: 20px;
+  justify-content: center;
+  align-items: center;
+  padding-top: 80px;
+`;
+
+const FoodCard = styled.div`
+  border: 0.66px solid;
+
+  border-image-source: radial-gradient(
+      80.69% 208.78% at 108.28% 112.58%,
+      #eabfff 0%,
+      rgba(135, 38, 183, 0) 100%
+    )
+    radial-gradient(
+      80.38% 222.5% at -13.75% -12.36%,
+      #98f9ff 0%,
+      rgba(255, 255, 255, 0) 100%
+    )
+    /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
+
+  width: 340px;
+  height: 167px;
+  flex-shrink: 0;
+  border-radius: 20px;
+  border: 0.659px solid #98f9ff;
+  background: url(<path-to-image>),
+    lightgray 0% 0% / 50.8334219455719px 50.8334219455719px repeat,
+    radial-gradient(
+      151.92% 127.02% at 15.32% 21.04%,
+      rgba(165, 239, 255, 0.2) 0%,
+      rgba(110, 191, 244, 0.04) 77.08%,
+      rgba(70, 144, 212, 0) 100%
+    );
+  background-blend-mode: overlay, normal;
+  backdrop-filter: blur(13.184196472167969px);
+
+  display: flex;
+  padding: 8px;
+
+  .food_info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: end;
+    h3 {
+      margin-top: 8px;
+      font-size: 16px;
+      font-weight: 500;
+    }
+
+    p {
+      margin-top: 4px;
+      font-size: 12px;
+    }
+  }
+`;
